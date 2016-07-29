b4f1ad3baf0543188c16879be8801d9e64f5c7ab
Anton Zhmyrov <Anton.Zhmyrov@teaminternational.com>
2016-07-29 17:44:57 +0300

    new project

---
+++ EssentialTools/Models/LinqValueCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EssentialTools.Models.Interfaces;
+
+namespace EssentialTools.Models
+{
+	public class LinqValueCalculator : IValueCalculator
+	{
+		public decimal ValueProducts(IEnumerable<Product> products)
+		{
+			return products.Sum(p => p.Price);
+		}
+	}
+}
