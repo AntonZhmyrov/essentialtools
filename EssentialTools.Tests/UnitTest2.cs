a84a4364764d0bfe11af231573ca181edc3fa339
Anton Zhmyrov <Anton.Zhmyrov@teaminternational.com>
2016-08-02 17:50:28 +0300

    Added moq unit tests

---
+++ EssentialTools.Tests/UnitTest2.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using EssentialTools.Models;
+using EssentialTools.Models.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EssentialTools.Tests
+{
+	/// <summary>
+	/// Summary description for UnitTest2
+	/// </summary>
+	[TestClass]
+	public class UnitTest2
+	{
+		private Product[] products =
+	    {
+			new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+			new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+			new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+			new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+	    };
+
+		[TestMethod]
+		public void Sum_Products_Correctly()
+		{
+			var mock = new Mock<IDiscountHelper>();
+			mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+			var target = new LinqValueCalculator(mock.Object);
+
+			var result = target.ValueProducts(products);
+
+			Assert.AreEqual(products.Sum(e => e.Price), result);
+		}
+
+		private Product[] CreateProduct(decimal value)
+		{
+			return new[] {new Product {Price = value}};
+		}
+	}
+}
